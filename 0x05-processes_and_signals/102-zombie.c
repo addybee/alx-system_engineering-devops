da176cb760d1be6d609caf25702a1c7da90dee5b
akinbode adeboyejo <akinbode.adeboyejo@gmail.com>
2024-02-26 22:35:13 -0800

    task 102

--- 0x05-processes_and_signals/102-zombie.c
+++ 0x05-processes_and_signals/102-zombie.c
@@ -2,19 +2,29 @@
 #include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
+
+/**
+ * infinite_while - sleeps indefinitely
+ * Return: 0 or 1
+ */
 int infinite_while(void)
 {
-    while (1)
-    {
-        sleep(1);
-    }
-    return (0);
+	while (1)
+	{
+		sleep(1);
+	}
+	return (0);
 }
-int main()
+
+/**
+ * main -  entry point
+ * Return: 0 for success or 1 (failure)
+ */
+int main(void)
 {
 	pid_t cpid;
 	int i;
-	
+
 	for (i = 0; i < 5; i++)
 	{
 		cpid = fork();
