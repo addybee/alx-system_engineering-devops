42f0d8723931bb3765a07fb931e8642d12664170
Adeboyejo Akinbode <akinbode.adeboyejo@gmail.com>
2024-04-19 18:26:59 +0100

    fix bug

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,10 +1,10 @@
 #create a file in /tmp.
 
 file{ '/tmp/school':
-  ensure  => present                #create a file in /tmp/school if absent.
-  path    => '/tmp/school'            #File path is /tmp/school
-  mode    => '0744'                   #File permission is 0744
-  owner   => 'www-data'               #File owner is www-data
-  group   => 'www-data'               #File group is www-data
-  content => 'I love Puppet'          #File contains I love Puppet
+  ensure  => file,                #create a file in /tmp/school if absent.
+  #path    => '/tmp/school'            #File path is /tmp/school
+  owner   => 'www-data',               #File owner is www-data
+  group   => 'www-data',               #File group is www-data
+  mode    => '0744',                   #File permission is 0744
+  content => 'I love Puppet',          #File contains I love Puppet
 }
