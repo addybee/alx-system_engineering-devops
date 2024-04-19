18343c7147a4b626d1d091cbb1080a411ef84f36
Adeboyejo Akinbode <akinbode.adeboyejo@gmail.com>
2024-04-19 21:17:25 +0100

    added full path

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,6 @@
 # install flask from pip3.
 
 exec { 'pip3 install Flask==2.1.0':
-  path   => '/usr/bin:/usr/sbin:/bin',
+  path   => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
   unless => '/usr/bin/pip3 show Flask | grep -q "^Version: 2.1.0$"',
 }
