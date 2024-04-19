73822c37836fb5261e76e78fedf8a4a54bf0372d
Adeboyejo Akinbode <akinbode.adeboyejo@gmail.com>
2024-04-19 21:50:54 +0100

    minor fix

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,12 @@
 # install flask from pip3.
 
-exec { 'pip3 install Flask==2.1.0':
-  path   => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
-  unless => '/usr/bin/pip3 show Flask | grep -q "^Version: 2.1.0$"',
+package {'Flask':
+  ensure   => '2.1.0',
+  provider => 'pip3',
+}
+
+package {'Werkzeug':
+  ensure   => '2.1.1',
+  provider => 'pip3',
+  before   => Package['Flask']
 }
