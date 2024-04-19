a50275d7e9184abdae683a5670b26a2736397f6e
Adeboyejo Akinbode <akinbode.adeboyejo@gmail.com>
2024-04-19 18:39:51 +0100

    added path

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -2,7 +2,7 @@
 
 file{ '/tmp/school':
   ensure  => file,                #create a file in /tmp/school if absent.
-  #path    => '/tmp/school'            #File path is /tmp/school
+  path    => '/tmp/school'            #File path is /tmp/school
   owner   => 'www-data',               #File owner is www-data
   group   => 'www-data',               #File group is www-data
   mode    => '0744',                   #File permission is 0744
