e70e3f6e676e9ae3b1cfe553dcb79d15141fc183
Adeboyejo Akinbode <akinbode.adeboyejo@gmail.com>
2024-04-30 02:42:06 +0100

    fix

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,34 +1,45 @@
 # configure a server
-$conf = "\tadd_header X-Served-By ${HOSTNAME};\n\t\\
-rewrite ^/redirect_me https://devhints.io/bash permanent;\n\t\\
-error_page 404 /404.html;\n\t\\
-location = /404.html {\n\t\t\\
-root /usr/share/nginx/html;\n\t\t\\
-internal;}\n"
+$add_header = 'sudo sed -i "/listen 80 default_server;/,/location \/ {/ { /location/a\\
+        add_header X-Served-By \"$HOSTNAME\";
+}" /etc/nginx/sites-available/default'
+
+$conf = 'sudo sed -i "/listen 80 default_server/a rewrite ^/redirect_me https://devhints.io/bash permanent;\\
+    error_page 404 /404.html;\\
+    location = /404.html {\\
+        root /usr/share/nginx/html;\\
+        internal;\\
+    }\n" /etc/nginx/sites-available/default'
+
 exec { 'update':
   command => 'sudo apt-get update',
   path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin'
 }
-package{'nginx':
+package{ 'nginx':
     ensure    => latest,
     provider  => 'apt',
     before    => Exec['update'],
 }
 
-file{'index_file'
+file{ 'index_file':
   path  => '/var/www/html/index.html'
   ensure  => file
   content => "Hello World!"
   before  => package['nginx']
 }
-file_line { 'custom_header':
-  path    => '/etc/nginx/sites-available/default',
-  line    => "${conf};",
-  after   => 'listen 80 default_server;',
+
+exec{ 'custom_header':
+  command => "${add_header}"
+  path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
   before  => File['index_file']
+}
+
+exec{ 'config':
+  command => "${conf}"
+  path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
+  before  => File['custom_header']
 }
 exec { 'restart':
   command => 'sudo service nginx restart',
   path    => '/usr/bin/',
-  before => File_line['custom_header']
+  before => File_line['config']
 }
